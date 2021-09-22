f894eb64f5a4b95cf8500afb56a5950c6e6da698
Ozan Sazak <sazak@Ozans-MacBook-Pro.local>
2021-09-23 01:36:10 +0300

    removed getConfigs function from the api client

--- edgedelta/api_client.go
+++ edgedelta/api_client.go
@@ -28,50 +28,6 @@ func (cli *EDConfigAPIClient) initializeHTTPClient() {
 	}
 }
 
-func (cli *EDConfigAPIClient) getConfigs() (GetAllConfigsResponse, error) {
-	cli.initializeHTTPClient()
-
-	baseURL, err := url.Parse(fmt.Sprintf("%s/v1/orgs/%s/confs", cli.APIBaseURL, cli.OrgID))
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest(http.MethodGet, baseURL.String(), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Add("Content-Type", "application/json")
-
-	fmt.Println(baseURL)
-	fmt.Println(req.URL.RequestURI())
-	fmt.Println(cli.apiKey)
-
-	req.Header.Add("Authorization", cli.apiKey)
-
-	resp, err := cli.cl.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("failed to do GET %s. error: %v", req.URL.RequestURI(), err)
-	}
-	defer resp.Body.Close()
-
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read response body from: %s. err: %v", req.URL.RequestURI(), err)
-	}
-
-	if 200 > resp.StatusCode || resp.StatusCode > 299 {
-		return nil, fmt.Errorf("got non OK http status from: %s, status: %v, response: %q", req.URL.RequestURI(), resp.StatusCode, string(b))
-	}
-
-	var responseData GetAllConfigsResponse
-	if err := json.Unmarshal(b, &responseData); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal the response body: %s", err)
-	}
-
-	return responseData, nil
-}
-
 func (cli *EDConfigAPIClient) getConfigWithID(configID string) (*GetConfigResponse, error) {
 	cli.initializeHTTPClient()
 
