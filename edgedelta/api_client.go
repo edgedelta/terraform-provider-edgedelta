c2f2a9a1e3a79e2af4992b3122d0b3da446d3595
Ozan Sazak <sazak@Ozans-MacBook-Pro.local>
2021-09-23 14:58:48 +0300

    updateConfigWithID implementation

--- edgedelta/api_client.go
+++ edgedelta/api_client.go
@@ -1,6 +1,7 @@
 package edgedelta
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -60,6 +61,50 @@ func (cli *ConfigAPIClient) getConfigWithID(configID string) (*GetConfigResponse
 	}
 
 	var responseData GetConfigResponse
+	if err := json.Unmarshal(b, &responseData); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal the response body: %s", err)
+	}
+
+	return &responseData, nil
+}
+
+func (cli *ConfigAPIClient) updateConfigWithID(configID string, configObject Config) (*UpdateConfigResponse, error) {
+	cli.initializeHTTPClient()
+
+	baseURL, err := url.Parse(fmt.Sprintf("%s/v1/orgs/%s/confs/%s", cli.APIBaseURL, cli.OrgID, configID))
+	if err != nil {
+		return nil, err
+	}
+
+	jsonData, err := json.Marshal(configObject)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal the config object: %v", err)
+	}
+
+	req, err := http.NewRequest(http.MethodPut, baseURL.String(), bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("X-ED-Config-Id", configID)
+
+	resp, err := cli.cl.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to do PUT %s. error: %v", req.URL.RequestURI(), err)
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body from: %s. err: %v", req.URL.RequestURI(), err)
+	}
+
+	if 200 > resp.StatusCode || resp.StatusCode > 299 {
+		return nil, fmt.Errorf("got non OK http status from: %s, status: %v, response: %q", req.URL.RequestURI(), resp.StatusCode, string(b))
+	}
+
+	var responseData UpdateConfigResponse
 	if err := json.Unmarshal(b, &responseData); err != nil {
 		return nil, fmt.Errorf("failed to unmarshal the response body: %s", err)
 	}
