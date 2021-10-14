55fae3c2a7015918e6d484349dd28f1490cf340d
Ozan Sazak <ozan.sazak@protonmail.ch>
2021-10-14 18:28:42 +0300

    different payload type template examples added

--- examples/monitors/multiple_monitors_with_shared_payload_template/edgedelta.tf
+++ examples/monitors/multiple_monitors_with_shared_payload_template/edgedelta.tf
@@ -24,18 +24,51 @@ variable "monitor_tags" {
     type = list(string)
 }
 
-data "template_file" "monitor_payloads" {
-    template = file("${path.module}/payload-template")
+data "template_file" "pattern_skyline_payloads" {
+    template = file("${path.module}/payload-template-skyline")
     for_each = var.monitor_tags
     vars = {
         tag = each.value
     }
 }
 
-resource "edgedelta_monitor" "existing_monitor_skyline" {
+data "template_file" "pattern_check_payloads" {
+    template = file("${path.module}/payload-template-check")
+    for_each = var.monitor_tags
+    vars = {
+        tag = each.value
+    }
+}
+
+data "template_file" "correlated_signal_payloads" {
+    template = file("${path.module}/payload-template-signal")
+    for_each = var.monitor_tags
+    vars = {
+        tag = each.value
+    }
+}
+
+resource "edgedelta_monitor" "monitor_pattern_skyline" {
     count    = length(var.monitor_tags)
-    name     = "skyline-example-monitor-${var.monitor_tags[count.index]}"
+    name     = "pattern-skyline-example-monitor-${var.monitor_tags[count.index]}"
     type     = "pattern-skyline"
     enabled  = true
-    payload  = data.template_file.monitor_payloads[count.index].rendered
+    payload  = data.template_file.pattern_skyline_payloads[count.index].rendered
+}
+
+
+resource "edgedelta_monitor" "monitor_pattern_check" {
+    count    = length(var.monitor_tags)
+    name     = "pattern-check-example-monitor-${var.monitor_tags[count.index]}"
+    type     = "pattern-check"
+    enabled  = true
+    payload  = data.template_file.pattern_check_payloads[count.index].rendered
+}
+
+resource "edgedelta_monitor" "monitor_correlated_signal" {
+    count    = length(var.monitor_tags)
+    name     = "correlated-signal-example-monitor-${var.monitor_tags[count.index]}"
+    type     = "correlated-signal"
+    enabled  = true
+    payload  = data.template_file.correlated_signal_payloads[count.index].rendered
 }
