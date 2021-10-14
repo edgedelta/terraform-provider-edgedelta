750fdc239f561d4f2f8cf4f7eb959be13c92f0bc
Ozan Sazak <ozan.sazak@protonmail.ch>
2021-10-14 11:29:24 +0300

    api client name generalization and function simplifications

--- edgedelta/provider.go
+++ edgedelta/provider.go
@@ -39,13 +39,13 @@ func Provider() *schema.Provider {
 }
 
 type ProviderMetadata struct {
-	client ConfigAPIClient
+	client APIClient
 }
 
 func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
 
 	return &ProviderMetadata{
-		client: ConfigAPIClient{
+		client: APIClient{
 			APIBaseURL: d.Get("api_endpoint").(string),
 			OrgID:      d.Get("org_id").(string),
 			apiSecret:  d.Get("api_secret").(string),
