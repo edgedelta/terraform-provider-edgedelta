d554e45387120fde9acd58043e0b8fccebf973c7
Onur Yilmaz <onury@edgedelta.com>
2025-11-28 18:04:50 +0300

    Terraform format

--- examples/monitors/correlated-signal/main.tf
+++ examples/monitors/correlated-signal/main.tf
@@ -1,29 +1,29 @@
 terraform {
-    required_providers {
-            edgedelta = {
-            source  = "edgedelta/edgedelta"
-            version = "0.0.7"
-        }
+  required_providers {
+    edgedelta = {
+      source  = "edgedelta/edgedelta"
+      version = "0.0.7"
     }
+  }
 }
 
 variable "ED_API_TOKEN" {
-    type = string
+  type = string
 }
 
 provider "edgedelta" {
-    org_id     = "<your-organization-id>"
-    api_secret = var.ED_API_TOKEN
+  org_id     = "<your-organization-id>"
+  api_secret = var.ED_API_TOKEN
 }
 
 output "monitor_id" {
-    value = edgedelta_monitor.example_monitor.id
+  value = edgedelta_monitor.example_monitor.id
 }
 
 resource "edgedelta_monitor" "example_monitor" {
-    name     = "correlated-signal-example-monitor"
-    type     = "correlated-signal"
-    enabled  = true
-    payload  = file("payload.json")
-    creator  = "creator@email.domain"
+  name    = "correlated-signal-example-monitor"
+  type    = "correlated-signal"
+  enabled = true
+  payload = file("payload.json")
+  creator = "creator@email.domain"
 }
