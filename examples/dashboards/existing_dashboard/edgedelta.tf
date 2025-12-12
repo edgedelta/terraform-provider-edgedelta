afa74d215b319b25b916b18fb3450e967bc022c7
Mustafa Soyvural <soyvural@gmail.com>
2025-12-12 17:11:13 +0300

    examples added

---
+++ examples/dashboards/existing_dashboard/edgedelta.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    edgedelta = {
+      source  = "edgedelta/edgedelta"
+      version = "0.0.9"
+    }
+  }
+}
+
+variable "ED_API_TOKEN" {
+  type = string
+}
+
+provider "edgedelta" {
+  org_id     = "<your-organization-id>"
+  api_secret = var.ED_API_TOKEN
+}
+
+output "dashboard_id" {
+  value       = edgedelta_dashboard.existing.dashboard_id
+  description = "The dashboard ID of the edgedelta_dashboard instance"
+}
+
+output "dashboard_name" {
+  value       = edgedelta_dashboard.existing.dashboard_name
+  description = "The name of the edgedelta_dashboard instance"
+}
+
+# Import an existing dashboard using: terraform import edgedelta_dashboard.existing <dashboard-id>
+resource "edgedelta_dashboard" "existing" {
+  dashboard_name = "My Existing Dashboard"
+  description    = "An existing dashboard managed via Terraform"
+  tags           = ["terraform", "imported"]
+
+  definition = file("/path/to/the/dashboard/definition/file.json")
+}
